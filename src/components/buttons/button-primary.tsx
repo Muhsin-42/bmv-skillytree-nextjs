cf5899e3243de3ccfa4248433b392cedafb6b61a
Muhsin-42 <muhsinofficial21@gmail.com>
2024-02-18 10:49:45 +0530

    Font family updated

--- src/components/buttons/button-primary.tsx
+++ src/components/buttons/button-primary.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ButtonPrimary = ({ text }: { text: string }) => {
   return (
-    <button className="primary-gradient h-[35px] w-[160px] rounded-[61px] font-bold text-white">
+    <button className="primary-gradient h-[35px] w-[160px] rounded-[61px] text-nav-active font-bold text-white">
       {text}
     </button>
   );
